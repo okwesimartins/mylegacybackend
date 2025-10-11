ffd2c44100271bf5dea200f6d9a46da809f85d29
unknown <okwesimartins@gmail.com>
2025-10-11 16:32:05 +0100

    new

--- app/Models/JournalAttachment.php
+++ app/Models/JournalAttachment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class JournalAttachment extends Model
 {
     protected $table = 'journal_attachments';
-
+    public $timestamps = false;
     protected $fillable = [
         'entry_id','stored_name','original_name','mime','size',
     ];
