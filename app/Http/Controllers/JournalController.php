8cfbf4e6617cdb8401ea2bcdd798fc3bda0abfe2
unknown <okwesimartins@gmail.com>
2025-10-11 16:03:46 +0100

    new

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -61,7 +61,7 @@ class JournalController extends Controller
         $validator = Validator::make($request->all(), [
             'id'          => 'nullable|integer|exists:journals,id',
             'name'        => 'required|string|max:255',
-            'template_id' => 'required|integer|exists:journaltemplate,id',
+            'template_id' => 'required|integer|exists:journal_template,id',
         ]);
 
         if ($validator->fails()) {
