f07db1ad4c62eb727a6e6e6c55311a893f29ef8a
unknown <okwesimartins@gmail.com>
2026-01-29 20:56:19 +0100

    new

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,7 +4,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Arr;
 
 class Plan extends Model
 {   
